5608c5baa6cfdef33d1f544cbb4e2e05cf8d7586
Zsolt <foldhazi.zsolt.2019@osztalyterem.katolikuskeri.hu>
2024-11-18 13:39:29 +0100

    0.0.1

--- main/frontend/frontend/src/app/app.routes.ts
+++ main/frontend/frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { KezdolapComponent } from './kezdolap/kezdolap.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: KezdolapComponent},
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
