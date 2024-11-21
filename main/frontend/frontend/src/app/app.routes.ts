76546b504b50d8a171cfa996a87c1672be86d6f6
Zsolt <foldhazi.zsolt.2019@osztalyterem.katolikuskeri.hu>
2024-11-21 11:14:22 +0100

    0.0.3

--- main/frontend/frontend/src/app/app.routes.ts
+++ main/frontend/frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 export const routes: Routes = [
     {path: '', component: HomepageComponent},
+    {path: '**', component: PageNotFoundComponent},
 
 ];
 
