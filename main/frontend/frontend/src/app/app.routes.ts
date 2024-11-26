0f93afb3e0f558fbb80fdea17dda81fdf31cda31
Zsolt <foldhazi.zsolt.2019@osztalyterem.katolikuskeri.hu>
2024-11-26 13:21:58 +0100

    0.0.7
    Elkészült a még nem teljesen reszponziv navigációs sáv, új megoldások implementálva lettek is rá.
    Jelenlegi munka hogy a routing működésre bírása a linkek funkcionalitása érdekében.

--- main/frontend/frontend/src/app/app.routes.ts
+++ main/frontend/frontend/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [
     {path: '', component: HomepageComponent},
-    {path: '**', component: PageNotFoundComponent},
+    {path: '**', component: PageNotFoundComponent},         /// Unknown routes redirection
+    {path: 'regist', component: RegistrationComponent},
+    {path: 'login', component: LoginComponent},
 
 ];
 
@@ -14,4 +18,4 @@ export const routes: Routes = [
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
