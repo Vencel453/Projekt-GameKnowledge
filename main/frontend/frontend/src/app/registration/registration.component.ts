4e3e49b3e95120c76b00eefce49cf5b9cf6c91fc
Zsolt <foldhazi.zsolt.2019@osztalyterem.katolikuskeri.hu>
2024-12-01 12:21:02 +0100

    0.0.12
    Frontend-backend kommunikácio regisztrációs oldalnál

--- main/frontend/frontend/src/app/registration/registration.component.ts
+++ main/frontend/frontend/src/app/registration/registration.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { response } from 'express';
 
 @Component({
   selector: 'app-registration',
@@ -8,9 +13,35 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
-  username: string = '';
-  password: string = '';
-  passagain: string = '';
-  email: string = '';
+  registForm: FormGroup;
+  successmess: string | null = null;
+  failuremess: string | null = null;
+  http: any;
+
+  constructor(private fb: FormBuilder){
+    this.registForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', [Validators.required, Validators.minLength(10)]],
+      passagain: ['', [Validators.required, Validators.minLength(10)]],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  onSubmit(){
+    if (this.registForm.valid){
+      this.http.post('https://localhost:3000', this.registForm.value)
+      .subscribe({
+        next: (response: any) =>{
+          if(response.status === 'success'){
+            this.successmess = response.message;
+            this.registForm.reset();
+          }else if(response.status === 'error'){
+              this.failuremess = response.message;
+              this.successmess = null;
+          }
+        }
+      })
+    }
+  }
 }
 
