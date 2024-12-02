14081c16a6710e891e1c2935528d4ddfd24b20fa
Vencel453 <vicicko.vencel.2019@osztalyterem.katolikuskeri.hu>
2024-12-02 08:53:47 +0100

    0.0.15
    Javítás

--- main/backend/src/config/config.js
+++ main/backend/src/config/config.js
@@ -1,9 +1,8 @@
-import { MariaDbDialect } from 'mariadb';
 // Az adatbázis konfigurációját külön fájlba mentjük, mert sok különböző fájlban fel van használva
 export const db_config = {
     username: 'root',
     password: '',
-    dialect: MariaDbDialect,
+    dialect: "mariadb",
     // Így a createdAt és updatedAt táblák automatikus létrehozását kikapcsoljuk, mert nincs rá szükségünk
     define: {
         timestamps: false,
