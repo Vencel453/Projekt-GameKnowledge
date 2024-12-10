049ceeeb44cbbfad4468cf0af8b7f9e9767922c8
Vencel453 <vicicko.vencel.2019@osztalyterem.katolikuskeri.hu>
2024-12-10 08:54:12 +0100

    0.0.13
    Javítások

--- main/frontend/frontend/src/app/registration/registration.component.ts
+++ main/frontend/frontend/src/app/registration/registration.component.ts
@@ -1,47 +1,101 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { response } from 'express';
+import { HttpClient,HttpErrorResponse, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule,ReactiveFormsModule, HttpClientModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
   registForm: FormGroup;
-  successmess: string | null = null;
-  failuremess: string | null = null;
-  http: any;
+  success: string | null = null;
+  failure: string | null = null;
 
-  constructor(private fb: FormBuilder){
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router){
     this.registForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(5)]],
-      password: ['', [Validators.required, Validators.minLength(10)]],
-      passagain: ['', [Validators.required, Validators.minLength(10)]],
-      email: ['', [Validators.required, Validators.email]]
-    })
+      username: [''],
+      email: [''],
+      password: [''],
+      passwordAgain: ['']
+    });
   }
 
   onSubmit(){
+    console.log('Form adatok:', this.registForm.value);
     if (this.registForm.valid){
-      this.http.post('https://localhost:3000', this.registForm.value)
+      this.http.post('https://localhost:3000/registration', this.registForm.value, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      }),
+      observe: 'response' as 'body'
+      })
       .subscribe({
         next: (response: any) =>{
-          if(response.status === 'success'){
-            this.successmess = response.message;
-            this.registForm.reset();
-          }else if(response.status === 'error'){
-              this.failuremess = response.message;
-              this.successmess = null;
+          if(response.error === "false"){
+            this.success = response.message;
+            this.failure === null;
+            setTimeout(() =>{
+              this.router.navigate(['/login']);
+            }, 2000);
           }
-        }
-      })
+        },
+        error:(err: HttpErrorResponse) =>{
+            this.success = null;
+    if(err.status === 400){
+      switch (err.error.message){
+        case "Not every parameter was filled!":
+          this.failure = "Not every parameter was filled!";
+          break;
+        case "The username is not in the correct length!":
+          this.failure = "The username is not in the correct length!";
+          break;
+        case "The passwords don't match!":
+          this.failure = "The passwords don't match!";
+          break;
+        case "The password isn't in a correct form!":
+          this.failure = "The password isn't in the correct form!";
+          break;
+        case "The email is in incorrect form!":
+          this.failure = "The email is in incorrect form!";
+          break;
+        default:
+          this.failure = "An unexpected error occured. Please try again later.";
+      }
+    }else if(err.status === 409){
+      this.failure = "A user with this username or email already exist!";
+    }else if(err.status === 500){
+      this.failure = "A server error occured. Please try again later.";
+    }else{
+      this.failure = "An unexpected error occured.";
+    }
+
+    console.error('Error:', err);
+  }
+});
     }
   }
 }
+  /*private handleError(err: HttpErrorResponse){
+    if(err.status === 400){
+      this.failuremess = err.error.message || 'Validation error occured.';
+  }else if(err.status === 409){
+    this.failuremess = "There's already a user with this username or email address!";
+  }else if(err.status === 500){
+    this.failuremess = "Something went wrong while creating an user!";
+  }else{
+    this.failuremess = 'Unexpected error occured.';
+  }
+  this.successmess = null;      
+    }
+  }*/
 
