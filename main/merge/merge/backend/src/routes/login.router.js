00444351ceff2a11c1e0783f509888ed5786af0b
Vencel453 <vicicko.vencel.2019@osztalyterem.katolikuskeri.hu>
2024-12-10 08:40:37 +0100

    Git rendezés 1

--- main/merge/merge/backend/src/routes/login.router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import loginController from "../controllers/login.controller.js";
-
-const LoginRouter = Router();
-
-LoginRouter.put("/login",loginController.LoginPutController);
-
-export default LoginRouter;
