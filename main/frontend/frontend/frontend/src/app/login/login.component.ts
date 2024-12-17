381d921e4a21f74878cb20183dedf4c6f1eecc5e
Zsolt <foldhazi.zsolt.2019@osztalyterem.katolikuskeri.hu>
2024-12-17 11:56:57 +0100

    0.1.0
    Megkezdődtek a Kezdőlapon való munkálatok.

---
+++ main/frontend/frontend/frontend/src/app/login/login.component.ts
@@ -0,0 +1,54 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { response } from 'express';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, HttpClientModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+errormess: string = '';
+successmess: string = '';
+username: string = '';
+password: string = '';
+
+constructor(private http: HttpClient, private router: Router){
+
+}
+
+onSubmit(): void {
+    this.http.post<any>(`http://localhost:3000/login`, {
+      username: this.username,
+      password: this.password
+    })
+    .subscribe({
+      next: (response) =>{
+        if(response.error === 'false'){
+          this.successmess = response.message;
+
+          localStorage.setItem('token', response.token);
+
+          this.router.navigate(['/']);
+        }
+      },
+      error: (err) => {
+        if(err.status === 400){
+          this.errormess = err.error.message;
+        }else if(err.status === 500){
+          this.errormess = err.error.message
+        }else{
+          this.errormess = 'An unknown error occured!';
+        }
+        console.error(err);
+      },
+    });
+  }
+}
---
+++ main/frontend/frontend/frontend/src/app/registration/registration.component.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { HttpClient,HttpErrorResponse, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [CommonModule, RouterModule,ReactiveFormsModule, HttpClientModule],
+  templateUrl: './registration.component.html',
+  styleUrl: './registration.component.scss'
+})
+export class RegistrationComponent {
+  registForm: FormGroup;
+  success: string | null = null;
+  failure: string | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router){
+    this.registForm = this.fb.group({
+      username: [''],
+      email: [''],
+      password: [''],
+      passwordAgain: ['']
+    });
+  }
+
+  onSubmit(){
+    console.log('Form adatok:', this.registForm.value);
+    if (this.registForm.valid){
+      this.http.post('http://localhost:3000/registration', this.registForm.value, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      }),
+      observe: 'response' as 'body'
+      })
+      .subscribe({
+        next: (response: any) =>{
+          if(response.error === "false"){
+            this.success = response.message;
+            this.failure === null;
+            setTimeout(() =>{
+              this.router.navigate(['/login']);
+            }, 2000);
+          }
+        },
+        error:(err: HttpErrorResponse) =>{
+            this.success = null;
+    if(err.status === 400){
+      switch (err.error.message){
+        case "Not every parameter was filled!":
+          this.failure = "Not every parameter was filled!";
+          break;
+        case "The username is not in the correct length!":
+          this.failure = "The username is not in the correct length!";
+          break;
+        case "The passwords don't match!":
+          this.failure = "The passwords don't match!";
+          break;
+        case "The password isn't in a correct form!":
+          this.failure = "The password isn't in the correct form!";
+          break;
+        case "The email is in incorrect form!":
+          this.failure = "The email is in incorrect form!";
+          break;
+        default:
+          this.failure = "An unexpected error occured. Please try again later.";
+      }
+    }else if(err.status === 409){
+      this.failure = "A user with this username or email already exist!";
+    }else if(err.status === 500){
+      this.failure = "A server error occured. Please try again later.";
+    }else{
+      this.failure = "An unexpected error occured.";
+    }
+
+    console.error('Error:', err);
+  }
+});
+    }
+  }
+}
+  /*private handleError(err: HttpErrorResponse){
+    if(err.status === 400){
+      this.failuremess = err.error.message || 'Validation error occured.';
+  }else if(err.status === 409){
+    this.failuremess = "There's already a user with this username or email address!";
+  }else if(err.status === 500){
+    this.failuremess = "Something went wrong while creating an user!";
+  }else{
+    this.failuremess = 'Unexpected error occured.';
+  }
+  this.successmess = null;      
+    }
+  }*/
+
