fa9376b863f1c90c1f013b2e9ea165aed4ae38b4
Zsolt <foldhazi.zsolt.2019@osztalyterem.katolikuskeri.hu>
2024-12-19 11:10:31 +0100

    0.1.2
    Kezdőlap továbbfolytatása, 90%-ban elkészült az első lista sor az új, már megjelent játékokról.

--- main/frontend/frontend/frontend/src/app/homepage/homepage.component.ts
+++ main/frontend/frontend/frontend/src/app/homepage/homepage.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
+    games = [
+      {name: 'Indiana Jones and the Great Circle', boxart: './indiana.jpg'},
+      {name: 'Alien: Rouge Incursion', boxart: './alien.avif'},
+      {name: 'Farming Simulator 25', boxart: './farming.avif'},
+      {name: 'Marvel Rivals', boxart: './marvel.jpg'},
+      {name: 'Stalker 2', boxart: './stalker.jpg'},
+      {name: 'Path of Exile 2', boxart: './pathofexile.png'},
+      {name: 'FairyTail 2', boxart: './fairytail2.png'},
+      {name: 'Antonblast', boxart: './antonblast.png'},
+    ];
 
+    leftscroll() {
+      const gamelist = document.querySelector('.gamelist') as HTMLElement;
+      gamelist.scrollBy({left: -300, behavior: 'smooth'});
+    }
+
+    rightscroll(){
+      const gamelist = document.querySelector('.gamelist') as HTMLElement;
+      gamelist.scrollBy({left: 300, behavior: 'smooth'});
+    }
 }
