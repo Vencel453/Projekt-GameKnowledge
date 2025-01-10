99ee18f20824ef4453733b9d9148bde3494d5d4e
Zsolt <foldhazi.zsolt.2019@osztalyterem.katolikuskeri.hu>
2025-01-10 13:07:14 +0100

    0.1.7
    Kezdolap footer része kész, valamint a minimenu tartalmai majdnem készek

--- main/frontend/frontend/frontend/src/app/app.component.ts
+++ main/frontend/frontend/frontend/src/app/app.component.ts
@@ -3,14 +3,20 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+  ismenuopen = false;
+
+  menutoggle(){
+    this.ismenuopen = !this.ismenuopen;
+  }
 }
