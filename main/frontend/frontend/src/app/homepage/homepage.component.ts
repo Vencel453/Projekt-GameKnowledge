0cd9c31e7322d9f96a1e0d24664c5cf9a02b35ec
Zsolt <foldhazi.zsolt.2019@osztalyterem.katolikuskeri.hu>
2025-01-28 12:22:28 +0100

    0.2.0
    Kezdolap funckionális, néhány stilusbeli modositásra van még szükség.

--- main/frontend/frontend/src/app/homepage/homepage.component.ts
+++ main/frontend/frontend/src/app/homepage/homepage.component.ts
@@ -1,16 +1,53 @@
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { response, Router } from 'express';
+import { release } from 'os';
+import { Routes, RouterModule } from '@angular/router';
+import { title } from 'process';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, HttpClientModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
 
     @ViewChildren('gamelist') gamelists!: QueryList<ElementRef>;
+
+    slideshow: any[] = [];
+    games: any = [];
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(){
+      this.loadGameData();
+    }
+
+    loadGameData(){
+      this.http.get<any>('http://localhost:3000/')
+        .subscribe((response: any) => {
+          if(!response.error) {
+            const datas = response.datas;
+            this.slideshow = response.datas.upcomingGames.map((game: any) =>({
+              id: game.id,
+              promoArt: game.promoArt,
+              title: `Game ${game.id}`,
+              release: game.release,
+            }));
+
+            this.games = response.datas.newReleaseGames.map((game: any) =>({
+              id: game.id,
+              boxart: game.boxart,
+              name: game.gameTitle,
+            }));
+          }else {
+            console.error('Error loading game data', response.messeage);
+          }
+        });
+    }
 
 
 
