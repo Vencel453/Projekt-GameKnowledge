72e5eacb4721d3f4dc994aa0ca1ec38c7745973c
Vencel453 <vicicko.vencel.2019@osztalyterem.katolikuskeri.hu>
2025-02-06 18:59:27 +0100

    0.3.4
    A validációk befejezve, az eddigi controller-ek át lettek írva ehhez kellően és befejeződött a Myprofile funkciói, mint a GET metódusra a lekérdezés és POST metódusra az datok változtatása.

--- main/backend/src/utilities/validation.methods.js
+++ main/backend/src/utilities/validation.methods.js
@@ -1,10 +1,10 @@
 export default {
-    CheckUsername: async(req) => {
-
+    CheckUsername: async(username) => {
+        return String(username).length >= 5 && String(username).length <= 30;
     },
 
-    CheckPassword: async(req) => {
-        
+    CheckPassword: async(password) => {
+        return String(password).length >= 10 && String(password).length <= 30 && !(password.toLowerCase() == password) && /[0-9]/.test(password);
     },
 
     CheckEmail: async(req) => {
